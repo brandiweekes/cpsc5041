88421a015618ca8ae9211da98ee1a78b1d08cc36
brandiweekes <brandiweekes@gmail.com>
2016-02-05 17:37:11 -0800

    INCOMPLETE: `added while loop for printing values

--- cpsc5041_hw2_program.cpp
+++ cpsc5041_hw2_program.cpp
@@ -11,14 +11,17 @@ int main()
 	int testScoreNameArrSize; 
 	string lastName;
 	int testScore;
+	int totalTestScores = 0;
 	int average = 0;
 	int[] TestScoreArr;
 	string[] TestNameArr;
+	int minPrint = 0;
+	int counter = 0;
 
 
 	
 	cout << "How many test scores will you enter?" << setw(4);
-	cin >> testScoreArrSize;
+	cin >> testScoreNameArrSize;
 
 	for(int i = 0; i < testScoreNameArrSize; i++)
 	{
@@ -36,15 +39,25 @@ int main()
 
 	cout << "Name" << setw(10) << "Score" << endl;
 
-	for (int i = 0; i < testScoreNameArrSize; i++)
+	while(counter < testScoreNameArrSize)
 	{
-		cout << TestNameArr[i] << setw(10) << TestScoreArr[i] << endl;
-		average += TestScoreArr[i]; 
+		for (int i = 0; i < testScoreNameArrSize; i++)
+		{
+			cout << TestNameArr[i] << setw(10) << TestScoreArr[i] << endl;
+			totalTestScores += TestScoreArr[i]; 
+		}
 	}
 
+	average = totalTestScores/testScoreArrSize;
+
 	cout << endl;
-	cout << "Average score:" << setw(4) << average/testScoreNameArrSize << endl;
+	cout << “Class average is ” << setprecision(2) << fixed << average << endl;
 
 	return 0;
+}
+
+void TestScoreNameArrays(int testScore, string lastName)
+{
+
 }
 
