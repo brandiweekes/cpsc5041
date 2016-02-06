8e9353b1e3260f2b47b42b93a4142741f0d2beac
brandiweekes <brandiweekes@gmail.com>
2016-02-05 19:06:40 -0800

    INC: added pointer logic for dynamic arrays

--- cpsc5041_hw2_program.cpp
+++ cpsc5041_hw2_program.cpp
@@ -3,8 +3,7 @@
 #include <iomanip>
 using namespace std;
 
-void TestScoreNameArrays(int testScore, string lastName);
-// string[] TestNameArr(string);
+void SortTestScoreNameArrays(int* testScoreArr, string* testNameArr);
 
 int main()
 {
@@ -13,42 +12,46 @@ int main()
 	int testScore;
 	int totalTestScores = 0;
 	int average = 0;
-	int[] TestScoreArr;
-	string[] TestNameArr;
-	int minPrint = 0;
-	int counter = 0;
-
+	int *testScoreArr;
+	string *testNameArr;
 
 	
 	cout << "How many test scores will you enter?" << setw(4);
 	cin >> testScoreNameArrSize;
 
-	for(int i = 0; i < testScoreNameArrSize; i++)
+	//dynamically allocate arrays & save address in pointer
+	testScoreArr = new int[testScoreNameArrSize];
+	testNameArr = new int[testScoreNameArrSize];
+
+	//initialize elements of arrays
+	for(int i = 1; i < testScoreNameArrSize; i++)
 	{
 		cout << "Enter student " << i << "'s name: ";
 		cin >> lastName;
 		cout << "Enter that student's test score: ";
 		cin >> testScore;
 
-		TestScoreNameArrays(testScore, lastName);
+		testScoreArr[i-1] = testScore;
+		testNameArr[i-1] = lastName;
 
 		cout >> "";
 	}
+
+	//sort arrays by test score in increasing order
+	SortTestScoreNameArrays(&testScoreArr, &testNameArr);
 
 	cout << "The test scores in ascending order, and their average, are: ";
 
 	cout << "Name" << setw(10) << "Score" << endl;
 
-	while(counter < testScoreNameArrSize)
-	{
-		for (int i = 0; i < testScoreNameArrSize; i++)
-		{
-			cout << TestNameArr[i] << setw(10) << TestScoreArr[i] << endl;
-			totalTestScores += TestScoreArr[i]; 
-		}
-	}
+	
+	for (int i = 0; i < testScoreNameArrSize; i++)
+	{		
+		cout << testNameArr[i] << setw(10) << testScoreArr[i] << endl;
+		totalTestScores += testScoreArr[i]; 
 
-	average = totalTestScores/testScoreArrSize;
+		//average = totalTestScores/testScoreArrSize;
+	}
 
 	cout << endl;
 	cout << “Class average is ” << setprecision(2) << fixed << average << endl;
@@ -56,8 +59,28 @@ int main()
 	return 0;
 }
 
-void TestScoreNameArrays(int testScore, string lastName)
+void SortTestScoreNameArrays(int* testScoreArr, string* testNameArr)
 {
+	int tempInt = 0;
+	string tempString = "";
+
+	for (int i = 0; i < testScoreNameArrSize - 1; i++)
+	{
+		for(int j = 1; j < testScoreArrSize; j++)
+		{
+			if(testScoreArr[i] > testScoreArr[j])
+			{
+				tempInt = *testScoreArr[i];				
+				*testScoreArr[i] = *testScoreArr[j];
+				*testScoreArr[j] = tempInt;
+
+				tempString = *testNameArr[i];
+				*testNameArr[i] = *testNameArr[j];
+				*testNameArr[j] = tempString;
+			}
+		}			
+	}
+
 
 }
 
