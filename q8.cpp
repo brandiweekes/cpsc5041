967b7f3c55f7d9199db2c6f31e33e603f5b6e88d
brandiweekes <brandiweekes@gmail.com>
2016-02-13 14:34:21 -0800

    INC: one last input bug input of the first entry's name is deleting the first character (no error on remaining 11 entries)
    Example--
    input: a
    prints:
    
    input: brandi
    prints: randi
    
    tested and confirmed it is the input -- cout << team[0].name confirmed result

--- q8.cpp
+++ q8.cpp
@@ -24,10 +24,18 @@ int main()
 	SoccerPlayer soccerTeam[12];
 
 	CreateTeam(soccerTeam);
+	cout << endl;
 
 	PrintRoster(soccerTeam);
+	cout << endl;
 
 	teamTotalPoints = TeamTotalPoints(soccerTeam);
+	cout << endl;
+
+	cout << "Total points earned by team: " << teamTotalPoints << endl;
+	cout << endl;
+
+	return 0;
 
 }
 
@@ -36,29 +44,34 @@ void CreateTeam(SoccerPlayer soccerTeam[])
 	//set properties for each soccer player in array
 	for(int i = 0; i < 12; i++)
 	{
-
 		//Get the soccer player's name
-		cout<<"Enter player " << i + 1 << "'s name: " << endl;
+		cout<<"Enter player " << i + 1 << "'s name: ";
 		cin.ignore();
 		getline(cin, soccerTeam[i].playerName);
 
 		//Get the soccer player's number
-		cout<<"Enter player " << i + 1 << "'s number: " << endl;
+		cout<<"Enter player " << i + 1 << "'s number: ";
 		cin >> soccerTeam[i].playerNum;
 
 		//Get the soccer player's points scored
-		cout<<"Enter player " << i + 1 << "'s points scored: " << endl;
+		cout<<"Enter player " << i + 1 << "'s points scored: ";
 		cin >> soccerTeam[i].playerPtScore;
 	}
 }
 
+//Summary: iterates through team to display: name, number, points
+//Input: array of 12 soccer players
+
 void PrintRoster(SoccerPlayer team[])
 {
 	cout << "Here is your team's roster:\n";
+	cout << endl;
+	//header for roster table
 	cout << setw(20) << left << "Name" << 
 	setw(20) << left << "Number" << 
 	setw(20) << left << "Points" << endl;
 
+	//iterates through each player in array to display: name, number, points
 	for(int i = 0; i < 12; i++)
 	{
 		cout << setw(20) << left << team[i].playerName << 
@@ -66,12 +79,19 @@ void PrintRoster(SoccerPlayer team[])
 		setw(20) << left << team[i].playerPtScore << endl;
 	}
 	cout << endl;
+
+	cout << team[0].playerName;
 }
 
+//Summary: adds the points scored by each player for a total team score
+//Input: array of 12 soccer players
+//Output: int totalPts
 int TeamTotalPoints(SoccerPlayer team[])
 {
+	//var to store addition of each player's points scored
 	int totalPts = 0;
 
+	//iterate through team to add each player's points scored to var totalPts
 	for(int i = 0; i < 12; i++)
 	{
 		totalPts += team[i].playerPtScore;
