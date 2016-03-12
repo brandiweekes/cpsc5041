45a9cc595952363f14ac9f9b3c3506cb6b10d5e6
brandiweekes <brandiweekes@gmail.com>
2016-03-12 11:14:13 -0800

    Update: Cache object of set objects instantiations complete
    cache configuration for direct, n-way associativity complete and debugged
    cache address breakdown by bits complete
    
    To Do: trace file ALGO

--- hw4.cpp
+++ hw4.cpp
@@ -108,54 +108,58 @@ void setupCacheWithSets(Cache c)
 	float offsetLog;
 	float setIndexLog;
 
+	offsetLog = (float)c.blockSize;		
+	c.blockOffsetBits = (int)log2(offsetLog);
+
 	if(c.associativity == 1) //fully associative
 	{
-		//fully associative cache of m blocks is an m-way set associative cache
-		c.numSets = c.blockSize; //** numSets is actually numBlocks (only 1 set)
-		
-		offsetLog = (float)c.blockSize;
-		c.setIndexBits = 0; //fully associative has ONLY 1 set
-		c.blockOffsetBits = (int)log2(offsetLog);
-		c.tagBits = 16 - c.blockOffsetBits;
-
-		//fully associative has ONLY 1 set
-		Set s;
-		s.blocks = new string*[c.numSets];
-		//instantiating array of block size (words per block)
-		cout << " inside block of fully associative " << endl;
-		for(int i = 0; i < c.numSets; i++)
-		{
-			s.blocks[i] = new string[c.blockSize];
-			
-			/* *****DEBUGGING***** */
-			cout << "i = " << i << endl;
-			for(int j = 0; j < c.blockSize; j++)
-			{
-				cout << "word " << j+1 << " " << endl;
-			}
-		}
-		cout << endl;
-	}
-	else if(c.associativity == 2) //2-way
-	{
-
-	}
-	else if(c.associativity == 4) //4-way
-	{
-
+		c.numSets = c.associativity;
+		c.associativity = c.blockSize; //n blocks = n-way associative
 	}
 	else if(c.associativity == 0) //direct mapped
 	{
-		// c.setIndexBits = ;
-		// c.tagBits = 
-		// c.blockOffsetBits = 
+		c.numSets = c.sizeDataPortion / c.blockSize;
+		c.associativity += 1; //1-way associative
 	}
-	else
+	else //n-way associative
 	{
-		cout<<"ERROR: Configuration File line 3 (must be value 0, 1, 2 or 4)"<<
-		endl;
+		c.numSets = c.sizeDataPortion / (c.blockSize * c.associativity);
 	}
+	setIndexLog = (float)c.numSets;
+	c.setIndexBits = (int)log2(setIndexLog);
+
+	c.tagBits = 16 - c.blockOffsetBits - c.setIndexBits;
+
+	//instantiating number of sets for cache
+	for(int i = 0; i < c.numSets; i++)
+	{
+		Set s;
+		s.blocks = new string*[c.numSets];
+		
+		/* *****DEBUGGING***** */
+		cout << " inside set " << i << " of setupCacheWithSets " << endl;
+		
+		//instantiating array of n-way set block size (words per block)
+		for(int j = 0; j < c.associativity; j++)
+		{
+			s.blocks[j] = new string[c.blockSize];
+			
+			/* *****DEBUGGING***** */
+			cout << "j = " << j << endl;
+			for(int j = 0; j < c.blockSize; j++)
+			{
+				cout << "word " << j+1 << " ";
+			}
+			cout << endl;
+		}
+		cout << endl;
+	}	
 }
+	// else
+	// {
+	// 	cout<<"ERROR: Configuration File line 3 (must be value 0, 1, 2 or 4)"<<
+	// 	endl;
+	// }
 
 void instantiateCache(int* ptrConfigArr, Cache c, int count)
 {
